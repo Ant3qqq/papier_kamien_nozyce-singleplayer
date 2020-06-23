72e914f1782b9be68be79cca573f0e6e2f93fd97
Ant3qqq <66523307+Ant3qqq@users.noreply.github.com>
2020-06-23 22:47:45 +0200

    działa klikanie jeszcze nic nie robi, html zakonczony

--- app.js
+++ app.js
@@ -1,3 +1,60 @@
 const userScore = 0;
 const computerScore = 0;
-const userScore_span = document.getElementById("wynikGracza")
+const userScore_span = document.getElementById("wynikGracza");
+const computerScore_span = document.getElementById("wynikKomputera");
+const scoreBoard_div = document.querySelector(".tablicaPunktow");
+const result_div = document.querySelector(".wynik");
+const rock_div = document.getElementById("k");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("n");
+
+function getComputerChoice(){
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3); 
+    return choices[randomNumber];
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    console.log("computer choice =>"+computerChoice);
+    console.log("user choice =>" + userChoice)
+}
+
+function main(){
+
+    rock_div.addEventListener('click', function() {
+        game("r");
+    });
+
+    paper_div.addEventListener('click', function() {
+        game("p");
+    });
+
+    scissors_div.addEventListener('click', function() {
+        game("s");
+    });
+}
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
