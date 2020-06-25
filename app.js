9212c61180e827a7af43b0d65efe0a98492719a1
Ant3qqq <66523307+Ant3qqq@users.noreply.github.com>
2020-06-25 22:20:54 +0200

    wssystko działa - trzeba dopieścić 1:14

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+var userScore = 0;
+var computerScore = 0;
 const userScore_span = document.getElementById("wynikGracza");
 const computerScore_span = document.getElementById("wynikKomputera");
 const scoreBoard_div = document.querySelector(".tablicaPunktow");
-const result_div = document.querySelector(".wynik");
+const result_p = document.querySelector(".wynik > p");
 const rock_div = document.getElementById("k");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("n");
@@ -14,26 +14,77 @@ function getComputerChoice(){
     return choices[randomNumber];
 }
 
+function convertToWord(letter){
+    switch(letter){
+        case "r":
+            return "Kamień";
+        case "p":
+            return "Papier";
+        case "s":
+            return "Nożyce";
+
+    }
+}
+
+function win(user, computer){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    //computerScore_span.innerHTML = computerScore;
+    const smallUser = "gracz".fontsize(3).sub();
+    const smallComp = "komputer".fontsize(3).sub();
+    if(user == "s"){
+        result_p.innerHTML = (convertToWord(computer) + smallUser + " biją " + convertToWord(computer) + smallComp +" Wygrywasz!");
+    } else {
+        result_p.innerHTML = (`${convertToWord(user)} ${smallUser} bije ${convertToWord(computer)} ${smallComp} Wygrywasz!`)
+    }
+    
+}
+
+function lose(user, computer){
+    computerScore++;
+    //userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUser = "gracz".fontsize(3).sub();
+    const smallComp = "komputer".fontsize(3).sub();
+    if(user == "s"){
+        result_p.innerHTML = (`${convertToWord(computer)} ${smallComp} biją ${convertToWord(user)} ${smallUser}  Przegrywasz :(`);
+    } else {
+        result_p.innerHTML = (`${convertToWord(computer)} ${smallComp} bije ${convertToWord(user)} ${smallUser} Przegrywasz :(`);
+    }
+   
+
+
+}
+
+function draw(user, computer){
+    result_p.innerHTML = "Remis";
+    const smallUser = "gracz".fontsize(3).sub();
+    const smallComp = "komputer".fontsize(3).sub();
+    if(user == "s"){
+        result_p.innerHTML = (`${convertToWord(computer)} ${smallComp} równają się ${convertToWord(user)} ${smallUser}  Remis`);
+    } else {
+        result_p.innerHTML = (`${convertToWord(computer)} ${smallComp} równa się ${convertToWord(user)} ${smallUser} Remis`);
+    }
+
+}
+
 function game(userChoice) {
     const computerChoice = getComputerChoice();
-    console.log("computer choice =>"+computerChoice);
-    console.log("user choice =>" + userChoice)
-    const name = "dupa";
     switch(userChoice + computerChoice){
         case "rs":
         case "pr":
         case "sp":
-            console.log("GRACZ WYGRYWA.");
+            win(userChoice, computerChoice);
             break;
         case "sr":
         case "rp":
         case "ps":
-            console.log("KOMPUTER WYGRYWA.");
+            lose(userChoice, computerChoice);
             break;
         case "rr":
         case "pp":
         case "ss":
-            console.log("REMIS");
+            draw(userChoice, computerChoice);
             break;
     }
 }
